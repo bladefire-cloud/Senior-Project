9de03fc4cc5d6f4232bb20f77d5e371f16642565
CecilJacobi <cjacobiv@gmail.com>
2023-10-02 13:15:46 -0500

    Pages created and login finished (for now).

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginPage from './Pages/LoginPage';
+import HomePage from './pages/HomePage';
 import { Routes,Route } from 'react-router-dom';
-
-
-
+import StatisticsPage from './Pages/StatisticsPage';
 
 function setToken(userToken) {
   console.log("tokenTEst")
@@ -18,14 +17,8 @@ function getToken() {
   return userToken?.token
 }
 
-
-
-
-
-
 function App() {
 
-  
   const token = getToken();
 
   if(!token){
@@ -39,12 +32,14 @@ function App() {
     );
 }
 
-
   return (
     <div >
      <Routes>
       <Route path="/" exact={true} index element={<LoginPage setToken={setToken}/>}/>
-     </Routes>
+      <Route path="/OneTimeSetupPage" exact={true} element={<OneTimeSetupPage/>}/>   
+      <Route path="/HomePage" exact={true} element={<HomePage/>}/>
+      <Route path="/StatisticsPage" exact={true} element={<StatisticsPage/>}/>
+    </Routes>
     </div>
   );
 }
